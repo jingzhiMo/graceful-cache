d605ce4f948c70ca43bcde467323c3afb06cc2f5
jingzhiMo <mojingzhi.mail@gmail.com>
2020-02-05 10:30:02 +0800

    docs(example): update example

--- example/index.ts
+++ example/index.ts
@@ -1,39 +1,47 @@
-import { MCache, clearStorage } from 'msn-cache'
+import { MCache } from 'msn-cache'
 
-var mc: MCache = new MCache({
-    name: 'LRU',
-    storage: 'localStorage',
-    capacity: 2
-})
-
-interface IVal {
-    value: string
-}
-var request = (): Promise<IVal> => {
-    return new Promise((resolve, reject) => {
-        console.log('need request')
-        setTimeout(() => {
-            resolve({
-                value: 'second value'
-            })
-        }, 1500)
-    })
-}
-
-mc.put('first', {
-    value: 'first value'
-})
-mc.get('first', () => {
-    console.log('use in first callback')
-    return {
-        first: 'first value'
+var mc: MCache = new MCache(
+    {
+        name: 'LRU',
+        capacity: 3
     }
-}).then(d => {
-    console.log('first', d)
+)
+
+async function start() {
+    console.log('start')
+    await mc.init()
+}
+
+var run = document.getElementById('run') as HTMLBaseElement
+var first = document.getElementById('first') as HTMLBaseElement
+var second = document.getElementById('second') as HTMLBaseElement
+var third = document.getElementById('third') as HTMLBaseElement
+var forth = document.getElementById('forth') as HTMLBaseElement
+
+first.addEventListener('click', async () => {
+    console.log('first', await mc.get('first', () => {
+        console.log('first: from network')
+        return 'first-value'
+    }, 1))
 })
 
-mc.get('second', () => {
-    return request()
-}).then(d => {
-    console.log('second', d)
+second.addEventListener('click', async () => {
+    // await mc.put('second', 'second-value')
+    console.log('second', await mc.get('second', () => {
+        console.log('second: from network')
+        return 'second-value'
+    }))
 })
+
+third.addEventListener('click', async () => {
+    // await mc.put('third', 'third-value')
+    console.log('third', await mc.get('third', () => {
+        console.log('third: from network')
+        return 'third-value'
+    }))
+})
+
+forth.addEventListener('click', async () => {
+    console.log('forth', await mc.get('forth'))
+})
+run.addEventListener('click', start)
